67d494c068bb67fd8a31b7863f721a2fe2956cb4
Prajna Shetty <prajnashetty340@gmail.com>
2018-07-18 23:09:45 +0530

    Initial commit

---
+++ app/src/main/java/com/example/prajnashetty/listapp/Contact.java
@@ -0,0 +1,28 @@
+package com.example.prajnashetty.listapp;
+
+/**
+ * Created by Prajna Shetty on 17-06-2018.
+ */
+
+public class Contact {
+
+        String name;
+        String image;
+        String phone;
+
+        public Contact() {
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public String getImage() {
+            return image;
+        }
+
+        public String getPhone() {
+            return phone;
+        }
+
+}
